623b9befbf589258a621fcf3c5db19ed451ba016
AlessandroBregoli <alessandroxciv@gmail.com>
2020-11-28 14:33:10 +0100

    Some graphical changes and [all] button.

--- styles/logbook/JS/logbook.js
+++ styles/logbook/JS/logbook.js
@@ -4,6 +4,12 @@ function show_div_in_content_visible(element){
     $("#content_visible").append(element.clone())
 }
 
+function show_all(){
+    $("#content_visible").empty();
+    $("<div id='content_visible'></div>").insertAfter("#table-of-contents")
+    $("#content_visible").append($("#content").children().clone())
+}
+
 function on_load(){
     $("<div id='main_container'></div>").prependTo("body")
     $("#table-of-contents").prependTo("#main_container");
@@ -11,16 +17,17 @@ function on_load(){
 
     $("#table-of-contents a").each(function(){
         $(this).attr("title", $(this).text())}
-                                  )
-
-    $("<div id='content_visible'></div>").insertAfter("#table-of-contents")
-    $("#content_visible").append($("#content").children().clone())
+                                  );
+    show_all();
 
     $("#table-of-contents a").click(function(e) {
         var tmp_var = $(this).attr("href").substr(1);
         tmp_var = "#outline-container-" + tmp_var
         show_div_in_content_visible($(tmp_var))
     })
+
+    $("<a onclick='show_all()' href='#'>[all]</a>").insertBefore("#text-table-of-contents")
+    $("#postamble").appendTo("#table-of-contents")
 
 }
 
