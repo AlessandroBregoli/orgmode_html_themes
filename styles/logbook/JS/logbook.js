5a643b878db554bf200251f4ebd6eb0573315a61
AlessandroBregoli <alessandroxciv@gmail.com>
2020-11-28 23:53:03 +0100

    Added effect on click

--- styles/logbook/JS/logbook.js
+++ styles/logbook/JS/logbook.js
@@ -8,28 +8,62 @@ function show_all(){
     $("#content_visible").empty();
     $("<div id='content_visible'></div>").insertAfter("#table-of-contents")
     $("#content_visible").append($("#content").children().clone())
+    $("#text-table-of-contents ul ul").show()
+    if(toc_active != null){
+        toc_active.removeClass("toc_active");
+        toc_active = null;
+    }
+}
+
+function event_onclick_toc(element){
+    var tmp_var = $(this).attr("href").substr(1);
+    tmp_var = "#outline-container-" + tmp_var
+    show_div_in_content_visible($(tmp_var))
+
+    $("#text-table-of-contents ul ul").hide()
+    recursive_show_toc($(this).parent())
+    $(this).parent().children().show()
+
+    if(toc_active != null){
+        toc_active.removeClass("toc_active")
+        toc_active = null;
+    }
+
+    toc_active = $(this);
+    toc_active.addClass("toc_active");
+}
+
+function recursive_show_toc(element){
+    if(element.is("li") || element.is("ul"))
+        recursive_show_toc(element.parent());
+    else
+        return;
+    if(element.is("ul"))
+        element.show();
 }
 
 function on_load(){
+    // Create the html structure
     $("<div id='main_container'></div>").prependTo("body")
     $("#table-of-contents").prependTo("#main_container");
     $(".title").prependTo("body");
 
-    $("#table-of-contents a").each(function(){
-        $(this).attr("title", $(this).text())}
-                                  );
+    //Show all the document inside the #content_visible div
     show_all();
 
-    $("#table-of-contents a").click(function(e) {
-        var tmp_var = $(this).attr("href").substr(1);
-        tmp_var = "#outline-container-" + tmp_var
-        show_div_in_content_visible($(tmp_var))
-    })
+    //TOC effects
+
+    $("#table-of-contents a").each(function(){
+        $(this).attr("title", $(this).text())
+    }
+                                  );
+    $("#table-of-contents a").click(event_onclick_toc);
+
 
     $("<a onclick='show_all()' href='#'>[all]</a>").insertBefore("#text-table-of-contents")
     $("#postamble").appendTo("#table-of-contents")
 
 }
 
-
+var toc_active = null;
 $(document).ready(on_load)
